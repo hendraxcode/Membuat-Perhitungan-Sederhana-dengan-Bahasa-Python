2298918768364090a95991026106e4e71bfbc668
Hendra Prayoga <56762256+hendraxcode@users.noreply.github.com>
2020-10-01 10:51:21 +0700

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -14,17 +14,19 @@ def pembagian (a,b):
 lagi= 'y'
 while lagi=='y':
     print ' \t \t Program Kalkulator Sayur'
-    a= input(' Masukan Bilangan 1: ')
-    b= input (' Masukan Biilangan 2 : ')
+    a= int(input(' Masukan Bilangan pertama : '))
+    b= int(input(' Masukan Biilangan kedua : '))
     print ' 1. Penjumlahan \n 2. pengurangan \n 3. perkalian \n 4. pembagian \n'
-    c= input ('Pilih 1-5 : ')
-    if c== 1:
+    pilihan = input ('Pilih 1-5 : ')
+    if pilihan == 1:
         print ' Hasilnya adalah = ', penjumlahan (a,b)
-    elif c== 2:
+    elif pilihan == 2:
         print ' Hasilnya adalah = ', pengurangan (a,b)
-    elif c==3 :
+    elif pilihan ==3 :
         print 'Hasilnya adalah = ', perkalian (a,b)
-    else :
+    elif pilihan ==4 :
         print 'Hasilnya adalah = ', pembagian (a,b)
+    else :
+        print 'masukan pilihan yang benar'
 
     lagi=raw_input(" Mau Lagi ?[y/t] : ")
